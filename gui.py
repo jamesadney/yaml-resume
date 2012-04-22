80e255f361cf939ae5ddc16c96b21d3ba2ba3cc7
James Adney <jfadney@gmail.com>
2012-04-22 03:13:02 -0700

    generate html file from gui

--- gui.py
+++ gui.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
+
+import tempfile
+import subprocess
+import os.path
+
 from gi.repository import Gtk
+import generator
+
+PDF_COMMAND = "wkhtmltopdf -s letter -B 1in -L 1in -R 1in -T .70in"
 
 class ResumeMaker():
 
@@ -12,14 +20,41 @@ class ResumeMaker():
     def _initialize_widgets(self):
         self.main_window = self.builder.get_object("main_window")
         self.file_chooser_btn = self.builder.get_object("filechooserbutton")
+        self.save_dialog = self.builder.get_object("filechooserdialog")
         self.html_btn = self.builder.get_object("html_button")
         self.pdf_btn = self.builder.get_object("pdf_button")
     
     def on_html_button_clicked(self, widget):
-        print self.file_chooser_btn.get_uri()
+        file_path = self.file_chooser_btn.get_filename()
+        html = self._generate_html(file_path)
+        response = self.save_dialog.run()
+        self.save_dialog.hide()
+        
+        html_file = self.save_dialog.get_filename()
+        with open(html_file, 'w') as f:
+            f.write(html)
+        
+        subprocess.call(['xdg-open', html_file])
     
     def on_pdf_button_clicked(self, widget):
-        pass
+        file_path = self.file_chooser_btn.get_filename()
+        print self._generate_pdf(file_path)
+            
+    def _generate_html(self, yaml_file):
+        html = generator.render_html(yaml_file)
+        return html
+    
+    def _generate_pdf(self, yaml_file):
+        html = self._generate_html(yaml_file)
+        temp = tempfile.NamedTemporaryFile(suffix=".html")
+        temp.write(html)
+        html_file = temp.name
+        pdf_file = os.path.splitext(temp.name)[0] + ".pdf"
+        
+        full_command = PDF_COMMAND.split() + [html_file, pdf_file]
+        subprocess.call(full_command)
+        
+        return pdf_file
 
 if __name__ == "__main__":
 
